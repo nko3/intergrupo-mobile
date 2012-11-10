01533bd2381543d32866dac6bf598e8561556955
Julian Duque <julianduquej@gmail.com>
2012-11-09 22:31:10 -0500

    Change database to jitsu mongohq

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports = config = {
   db: {
-    url: "mongodb://node:knockout@alex.mongohq.com:10017/canvasmodel"
+    url: "mongodb://nodejitsu:2bb3f8f3e1924aa8cebc947fd75bcf78@alex.mongohq.com:10094/nodejitsudb3653017159"
   }
 };
