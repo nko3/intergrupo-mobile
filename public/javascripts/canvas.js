152887bf2ed580877aa9e9add3799cc50af38473
jaiversin <jaiversin@gmail.com>
2012-11-10 17:41:20 -0500

    First steps on canvas. Merged canvas data id

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,3 +1,73 @@
+var drawTemplate = function (canvas, metadata){
+  console.log(metadata);
+};
+
+var dragStopped = function(object){
+  //Emit
+  // console.log(object.id);
+};
+
+var dragStarted = function(object){
+  //Emit
+  // console.log(object.id);
+};
+
 (function() {
 
+  var c = $('canvas'); 
+
+
+
+
+  c.drawArc({
+    layer: true,
+    fillStyle: "#36b",
+    strokeStyle: '#36b',
+    strokeWidth: 2,
+    x: 150, y: 150,
+    radius: 50,
+    ccw: true,
+    draggable: true,
+    bringToFront: true,
+    id: "ootro",
+    dragstop: dragStopped,
+    dragstart: dragStarted
+  })
+  .drawRect({
+    layer: true,
+    fillStyle: "#6c1",
+    strokeStyle: '#6c1',
+    strokeWidth: 2,
+    x: 100, y: 100,
+    width: 100, height: 100,
+    draggable: true,
+    bringToFront: true,
+    id: "algun_id",
+    dragstop: dragStopped,
+    dragstart: dragStarted
+  });
+
+  drawTemplate(c, {});
+
+
+  //Make canvas of variable width
+  
+  var ct = c.get(0).getContext('2d'); 
+  var container = $(c).parent(); 
+  //Run function when browser resizes 
+  $(window).resize( respondCanvas ); 
+  function respondCanvas(){ 
+    c.attr('width', $(container).width() ); 
+    //max width 
+    c.attr('height', $(container).height() ); 
+    //max height 
+    //Call a function to redraw other content (texts, images etc) 
+    c.drawLayers();
+  } 
+    //Initial call 
+  respondCanvas();
+  
+
 })();
+
+
