6618d795e69ac9ba761fa59223df5e01edb81634
jaiversin <jaiversin@gmail.com>
2012-11-11 10:49:15 -0500

    Fixed postit rotation. CC @betancur jajaja

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,134 +1,31 @@
-var drawTemplate = function (canvas, metadata){
-  console.log(metadata);
-};
-
-var dragStopped = function(layer){
-  //Emit for unlock and update
-  // console.log(object.id);
-  var canvas = $('canvas');
-  var text_layer = canvas.getLayer(layer.text);
-  text_layer.x = layer.x + 5;
-  text_layer.y = layer.y + 20;
-  text_layer.opacity = 1;
-  var degrees = Math.floor(Math.random() * 7) - 6
-  text_layer.rotate = degrees;
-  layer.rotate = degrees;
-};
-
-var dragStarted = function(layer){
-  var canvas = $('canvas');
-  var text_layer = canvas.getLayer(layer.text);
-  text_layer.opacity = 0;
-
-  //Emit for lock
-  // console.log(object.id);
-};
-
-
-var generateId = function (bits) {
-  var chars, rand, i, ret;
-
-  chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
-  ret = '';
-
-  // in v8, Math.random() yields 32 pseudo-random bits (in spidermonkey it gives 53)
-  while (bits > 0) {
-    // 32-bit integer
-    rand = Math.floor(Math.random() * 0x100000000);
-    // base 64 means 6 bits per character, so we use the top 30 bits from rand to give 30/6=5 characters.
-    for (i = 26; i > 0 && bits > 0; i -= 6, bits -= 6) {
-      ret += chars[0x3F & rand >>> i];
-    }
-  }
-
-  return ret;
-};
-
-var drawPostit = function(canvas, metadata){
-  var post_name = generateId(32);
-  var text_name = generateId(32);
-  var close_name = generateId(32);
-  var group_name = generateId(32);
-
-  var postData = {
-      layer: true,
-      id: post_name,
-      name: post_name,
-      text: text_name,
-      close: close_name,
-      group: group_name,
-      fillStyle: metadata.fillStyle,
-      strokeStyle: metadata.strokeStyle,
-      strokeWidth: 2,
-      x: (parseInt(canvas.attr("width")) / 2), y: (parseInt(canvas.attr("height")) / 2),
-      width: metadata.size, height: metadata.size,
-      draggable: true,
-      bringToFront: false,
-      dragstop: dragStopped,
-      dragstart: dragStarted,
-      click: function (layer){
-        //console.log(layer.group);
-      },
-      mouseover: function (layer){
-        $(this).css('cursor','move');
-      }, 
-      mouseout: function (layer){
-        $(this).css('cursor','auto');
-      }
-    };
-  canvas.drawRect(postData);
-  canvas.drawText({
-    layer: true,
-    name: text_name,
-    id: text_name,
-    group: group_name,
-    fillStyle: "#000",
-    strokeWidth: 2,
-    draggable: false,
-    bringToFront: true,
-    x: postData.x + 5, y: postData.y + 20,
-    height: postData.height,
-    maxWidth: postData.width - 10,//Beta
-    font: "bold 14pt Trebuchet MS",
-    text: "Add text here bla bla bla",
-    click: function(layer){
-      var textPrompted = prompt("Edit the text", layer.text);
-      if (textPrompted) {
-        layer.text = textPrompted;
-      }
-      
-    },
-    mouseover: function (layer){
-      $(this).css('cursor','text');
-    },
-    mouseout: function (layer){
-      $(this).css('cursor','move');
-    }
-  });
-
-  // canvas.drawText({
-  //   layer: true,
-  //   name: close_name,
-  //   id: close_name, 
-  //   group: group_name,
-
-  // });
-};
-
 (function() {
 
-  var c = $('canvas'); 
+  var c = $('canvas');
   
+  var socket = io.connect('http://localhost:3000/canvas');
+
+
+  socket.on('connect', function() {
+    console.log("connected");
+  });
+
+  socket.on('element added', function(element){
+    console.log(element);
+    drawPostit(c, element);
+  });
 
   $("#add_postit").click(function (e) {
     e.preventDefault();
 
-    drawPostit(c, {fillStyle: "#909", strokeStyle: "#F99", size: 100});
+    var element = drawPostit(c, {fillStyle: "#909", strokeStyle: "#F99", size: 100});
+    socket.emit("add element", element);
+  });
 
+  c.hover(function() {
+    $(this).css('cursor','auto');
   });
 
   //Make canvas of variable width
-  
   var ct = c.get(0).getContext('2d'); 
   var container = $(c).parent(); 
   //Run function when browser resizes 
@@ -145,6 +42,140 @@ var drawPostit = function(canvas, metadata){
     //Initial call 
   respondCanvas();
   
+
+  var drawTemplate = function (canvas, metadata){
+    console.log(metadata);
+  };
+
+  var dragStopped = function(layer){
+    //Emit for unlock and update
+    // console.log(object.id);
+    var canvas = $('canvas');
+    var text_layer = canvas.getLayer(layer.text);
+    text_layer.x = layer.x + 5;
+    text_layer.y = layer.y + 10;
+    text_layer.opacity = 1;
+    var degrees = Math.floor(Math.random() * 12) - 6;
+    console.log()
+    text_layer.rotate = degrees;
+    layer.rotate = degrees;
+  };
+
+  var dragStarted = function(layer){
+    var canvas = $('canvas');
+    var text_layer = canvas.getLayer(layer.text);
+    text_layer.opacity = 0;
+
+    //Emit for lock
+    // console.log(object.id);
+  };
+
+
+  var generateId = function (bits) {
+    var chars, rand, i, ret;
+
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
+    ret = '';
+
+    // in v8, Math.random() yields 32 pseudo-random bits (in spidermonkey it gives 53)
+    while (bits > 0) {
+      // 32-bit integer
+      rand = Math.floor(Math.random() * 0x100000000);
+      // base 64 means 6 bits per character, so we use the top 30 bits from rand to give 30/6=5 characters.
+      for (i = 26; i > 0 && bits > 0; i -= 6, bits -= 6) {
+        ret += chars[0x3F & rand >>> i];
+      }
+    }
+
+    return ret;
+  };
+
+  var drawPostit = function(canvas, metadata){
+    var post_name = generateId(32);
+    var text_name = generateId(32);
+    var close_name = generateId(32);
+    var group_name = generateId(32);
+
+    var postData = {
+        layer: true,
+        id: post_name,
+        name: post_name,
+        text: text_name,
+        close: close_name,
+        group: group_name,
+        fillStyle: metadata.fillStyle,
+        strokeStyle: metadata.strokeStyle,
+        strokeWidth: 2,
+        x: (parseInt(canvas.attr("width")) / 2), y: (parseInt(canvas.attr("height")) / 2),
+        width: metadata.size, height: metadata.size,
+        draggable: true,
+        bringToFront: false,
+        dragstop: dragStopped,
+        dragstart: dragStarted,
+        click: function (layer){
+          //console.log(layer.group);
+        },
+        mouseover: function (layer){
+          $(this).css('cursor','move');
+        }, 
+        mouseout: function (layer){
+          $(this).css('cursor','auto');
+        }
+      };
+    canvas.drawRect(postData);
+    canvas.drawText({
+      layer: true,
+      name: text_name,
+      id: text_name,
+      group: group_name,
+      fillStyle: "#000",
+      strokeWidth: 2,
+      draggable: false,
+      bringToFront: true,
+      x: postData.x + 5, y: postData.y + 10,
+      height: postData.height,
+      maxWidth: postData.width - 10,//Beta
+      font: "bold 14pt Trebuchet MS",
+      text: "Add text here bla bla bla",
+      click: function(layer){
+        var textPrompted = prompt("Edit the text", layer.text);
+        if (textPrompted) {
+          layer.text = textPrompted;
+        }
+        
+      },
+      mouseover: function (layer){
+        $(this).css('cursor','text');
+      },
+      mouseout: function (layer){
+        $(this).css('cursor','move');
+      }
+    });
+
+    // canvas.drawText({
+    //   layer: true,
+    //   name: close_name,
+    //   text_layer: text_name,
+    //   id: close_name, 
+    //   group: group_name,
+    //   fillStyle: "#333",
+    //   strokeWidth: 3,
+    //   strokeStyle: "#333",
+    //   draggable: false,
+    //   bringToFront: true,
+    //   x: postData.x - 35, y: postData.y - 35,
+    //   height: 10,
+    //   maxWidth: 10,
+    //   font: "bold 12pt Trebuchet MS",
+    //   text: "X",
+    //   click: function (layer){
+    //     canvas.removeLayerGroup(group_name);
+    //     canvas.removeLayer(layer.text_layer);
+    //     canvas.drawLayers();
+    //   }
+    // });
+    return postData;
+  };
 
 })();
 
