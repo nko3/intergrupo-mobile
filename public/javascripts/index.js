dc20b263b4e5adc59480da12accaaa3a48b31996
Juan Pablo Lopez <dolcalmi@gmail.com>
2012-11-11 01:31:13 -0500

    add type param to create request

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,8 @@
 
 $(document).ready(function () {
     $('.roundabout').roundabout();
+    $('.canvas-menu a').click(function () {
+        $('#modalTitle').text($(this).attr('title'));
+        $('input[name=type]').val($(this).attr('rel'));
+    });
 });
