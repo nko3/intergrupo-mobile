814b583e6acb84805f389016a3d4cf3a02b2e01e
Juan Pablo Lopez <dolcalmi@gmail.com>
2012-11-11 15:02:50 -0500

    final changs home page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,15 @@
 
 $(document).ready(function () {
-    $('.roundabout').roundabout();
+    $('.roundabout').roundabout({
+        clickToFocusCallback: function () {
+            $(".span4 h4").removeClass('bg-highlight');
+            $('.' + $('.roundabout-in-focus').find('a').attr('rel') + ' h4').addClass('bg-highlight');
+        }
+
+    });
+
+
+
     $('.canvas-menu a').click(function () {
         $('#modalTitle').text($(this).attr('title'));
         $('input[name=type]').val($(this).attr('rel'));
